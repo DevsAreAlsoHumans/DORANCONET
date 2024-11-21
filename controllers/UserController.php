4aa134e6479b6bfe71303c74c9e23c953a74848b
stradimarius <mariusclement68@gmail.com>
2024-11-21 16:02:28 +0100

    user model

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,9 +1,23 @@
 <?php
 
+use Models\User;
+
 class UserController {
 
     public function register() {
-        
+        $user = new User(
+            0,
+            $_POST["first_name"],
+             $_POST["last_name"], 
+             $_POST["trip-start"], 
+             $_POST["email"],
+              $_POST["password"],
+              $_POST["profile_picture"],
+              true,
+              new DateTime,
+            );
+
+            $user->register(); 
     }
 }
 
