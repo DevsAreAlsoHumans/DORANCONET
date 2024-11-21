aff5b787ca18f4e3b23a99254218f44668d1bb86
stradimarius <mariusclement68@gmail.com>
2024-11-21 12:24:54 +0100

    user model

--- models/UserModel.php
+++ models/UserModel.php
@@ -38,6 +38,77 @@ class User
     public function getId() {
         return $this->id;
     }
+    public function getLastName() {
+        return $this->last_name;
+    }
+
+    public function getFirstName() {   
+        return $this->first_name;
+    }
+
+    public function getDateOfBirth() {
+        return $this->date_of_birth;    
+    }
+
+    public function getEmail() {
+        return $this->email;    
+    }
+
+    public function getProfilPicture() { 
+        return $this->profil_picture;
+    }   
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function isActive() { 
+        return $this->is_active;
+    }   
+
+    public function isDeleted() {
+        return $this->deleted_at;
+    }
+    
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setLastName($last_name) {
+        $this->last_name = $last_name;
+    }
+
+    public function setFirstName($first_name) {
+        $this->first_name = $first_name;
+    }
+
+    public function setDateOfBirth($date_of_birth) {    
+        $this->date_of_birth = $date_of_birth;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setProfilPicture($profil_picture) {
+        $this->profil_picture = $profil_picture;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;   
+    }
+
+    public function setIsActive($is_active) {
+        $this->is_active = $is_active;
+    }
+
+    public function setDeletedAt($deleted_at) {
+        $this->deleted_at = $deleted_at;
+    }
+
+
+
+
 
     public function register() {
 
