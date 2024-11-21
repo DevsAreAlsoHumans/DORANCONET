446f2437d3bb3f31f0c7958c0a8c1b64d980656d
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2024-11-21 14:15:24 +0100

    check if session start

--- views/login.php
+++ views/login.php
@@ -1,50 +1,57 @@
 <!DOCTYPE html>
-  <html>
+<html>
 
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="color-scheme" content="light dark" />
-    <title>Connection</title>
-    <link rel="stylesheet" href="../public/assets/css/login.css">
-    <link rel="stylesheet" href="../public/assets/css/style.css">
-  </head>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="color-scheme" content="light dark" />
+  <title>Connection</title>
+  <link rel="stylesheet" href="../public/assets/css/login.css">
+  <link rel="stylesheet" href="../public/assets/css/style.css">
+</head>
 
-  <body>
-    <header>
-      <img src="../public/assets/img/doranco_image.png" alt="logo doranco" />
-    </header>
-    <main>
+<body>
+  <?php
+  if (session_id() == '') {
+    session_start();
+  }
+  ?>
 
-      <form action="" method="post">
 
-        <div>
-          <label for="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value="<?php echo isset($_SESSION['form_data']['email']) ? htmlspecialchars($_SESSION['form_data']['email']) : ''; ?>">
-          <small>
-            <?php #handle email error 
-            ?>
-            <p>error</p>
-          </small>
-        </div>
-        <div>
-          <label for="password">Mot de passe</label>
-          <input type="password" name="password" id="password">
-          <small>
-            <?php #handle password error 
-            ?>
-          </small>
-        </div>
+  <header>
+    <img src="../public/assets/img/doranco_image.png" alt="logo doranco" />
+  </header>
+  <main>
 
-        <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']?>">
-        
-        <button type="submit">Se Connecter</button>
-      </form>
-    </main>
-  </body>
+    <form action="" method="post">
 
-  </html>
+      <div>
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value="<?php echo isset($_SESSION['form_data']['email']) ? htmlspecialchars($_SESSION['form_data']['email']) : ''; ?>">
+        <small>
+          <?php #handle email error 
+          ?>
+          <p>error</p>
+        </small>
+      </div>
+      <div>
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password">
+        <small>
+          <?php #handle password error 
+          ?>
+        </small>
+      </div>
+
+      <input type="hidden" name="csrf_token" value="<?php echo isset($_SESSION['csrf_token']) ? $_SESSION['csrf_token'] : "" ?>">
+
+      <button type="submit">Se Connecter</button>
+    </form>
+  </main>
+</body>
+
+</html>
