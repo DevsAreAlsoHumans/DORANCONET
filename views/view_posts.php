38312cd238495c8efba955f0e1187c6db101ac6d
FranckR77 <franck.reignier77@gmail.com>
2024-11-22 09:18:11 +0100

    Update view_posts.php v2

--- views/view_posts.php
+++ views/view_posts.php
@@ -32,14 +32,18 @@ $posts = array(
 
 <div class="posts">
     <?php
-    foreach ($posts as $post) {
-        echo "<div class='post'>";
-        echo "<div class='name_user'> <p>" . htmlspecialchars($post["first_name"]) . " " . htmlspecialchars($post["last_name"]) . "</p> </div>";
-        echo "<div class='post_content'> <p>" . htmlspecialchars($post["content"]) . "</p> </div>";
-        echo "<img class='post_image' src='" . htmlspecialchars($post["image_path"]) . "'>";
-        echo "<div class='post_likes'> <p>" . "<img class='heart_likes' src=../public/assets/img/heart.svg> " . htmlspecialchars($post["likes"]) . "</p> </div>";
-        echo "<div class='post_created_at'> <p>" . "<img class='calendar' src=../public/assets/img/calendar.svg> " . htmlspecialchars($post["created_at"]) . "</p> </div>";
-        echo "</div>";
+    if (isset($posts)) {
+        foreach ($posts as $post) {
+            echo "<div class='post'>";
+            echo "<div class='name_user'> <p>" . htmlspecialchars($post["first_name"]) . " " . htmlspecialchars($post["last_name"]) . "</p> </div>";
+            echo "<div class='post_content'> <p>" . htmlspecialchars($post["content"]) . "</p> </div>";
+            echo "<img class='post_image' src='" . htmlspecialchars($post["image_path"]) . "'>";
+            echo "<div class='post_likes'> <p>" . "<img class='heart_likes' src=../public/assets/img/heart.svg> " . htmlspecialchars($post["likes"]) . "</p> </div>";
+            echo "<div class='post_created_at'> <p>" . "<img class='calendar' src=../public/assets/img/calendar.svg> " . htmlspecialchars($post["created_at"]) . "</p> </div>";
+            echo "</div>";
+        }
+    } else {
+        echo "Aucun post publié";
     }
     ?>
 </div>
